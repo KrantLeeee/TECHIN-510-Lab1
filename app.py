2cbe71fc2e401710409e4a2b53ea4a5bb126e726
Krant Lee <krantlee@Krants-MacBook-Pro.local>
2024-03-26 16:51:39 -0700

    web2

--- app.py
+++ app.py
@@ -7,28 +7,32 @@ col1, col2 = st.columns(2)
 with col1:
     # About me
     st.header('About Me')
-    st.write("I'm equipped with a heart of a dreamer and the mind of a realist. "
-             "I am passionate about pioneering in the realm of AI 🤖, specifically in crafting natural language interaction interfaces that seamlessly connect human intuition with AI 🧠."
-)
+    st.write("I'm equipped with a heart of a dreamer and the mind of a realist.\n"
+             "I am passionate about pioneering in the realm of AI 🤖, specifically in crafting natural language interaction interfaces that seamlessly connect human intuition with AI 🧠.")
 
     # Education
     st.header('Education')
-    st.write("I'm currently a M.S. graduate student at UW ↗"
-"I have a B.A. in Art History at Sichuan University ↗"
-"Contact me via LinkedIn ↗")
+    if st.button('M.S. graduate student at UW'):
+        st.markdown("https://gix.uw.edu/program/msti/", unsafe_allow_html=True)
+    if st.button('B.A. in Art History at Sichuan University'):
+        st.markdown("https://jwc.scu.edu.cn/", unsafe_allow_html=True)
+    if st.button('Contact me via LinkedIn'):
+        st.markdown("http://www.linkedin.com/in/krant-lee1", unsafe_allow_html=True)
 
     # Hobbies
     st.header('Fun Facts About Me!')
-    st.write("I was an artist 👨🏻‍🎨"
-             "I am a skilled badminton player 🏸"
-             "I am a value investor 💵"
-             "I am addicted to music 🎸, detective novel 🕵🏻‍♂️ and philosophy 🧐")
+    st.write("I was an artist 👨🏻‍🎨")
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.write("I am a skilled badminton player 🏸")
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.write("I am a value investor 💵")
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.write("I am addicted to music 🎸, travel 🚗, detective novel 🕵🏻‍♂️ and philosophy 🧐")
 
-
-# 在右列添加图片和视频
+# Add img & video
 with col2:
-    # 个人资料图片
     st.image('my avatar1.jpg', caption='My Recent Photo', use_column_width=True)
-    
-    # 添加一段视频
+
+    st.image('My Painting.jpg', caption='My Painting', use_column_width=True)
+
     st.video('walking.mov')
